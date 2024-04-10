4dfb9d2ec87fdbd67c87276ab5fbe5a5e8cb38c6
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-10 12:42:45 -0400

    js add img and win text on button push, style linked

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
 const buttonElement = document.querySelector('#buzzer');
+const headElement = document.querySelector('head');
+const gameElement = document.querySelector('#game');
+
 let buttonPushed = false;
 
 const buttonHandler = () => {
-    if (!buttonPushed)
-        console.log('do the thing');
+    if (!buttonPushed) {
+        const cookieElement = document.createElement('img');
+        cookieElement.setAttribute('id', 'cookie');
+        cookieElement.setAttribute('src', 'https://www.thedailymeal.com/img/gallery/chocolate-chip-cookies-recipe/intro-1665769539.webp');
+        cookieElement.setAttribute('alt', 'Picture of cookie');
+
+        const winElement = document.createElement('h2');
+        winElement.setAttribute('id', 'win-text');
+        winElement.innerHTML = 'You won! Enjoy your cookie!';
+
+        const cookieStyleElement = document.createElement('link');
+        cookieStyleElement.setAttribute('rel', 'stylesheet');
+        cookieStyleElement.setAttribute('href', 'css/cookie.css');
+
+        gameElement.appendChild(cookieElement);
+        gameElement.appendChild(winElement);
+        headElement.appendChild(cookieStyleElement);
+
+        buttonPushed = true;
+    }
     else
         console.log('stop pushing the button! nooo more cookies!');
-
-    buttonPushed = true;
 }
 
 buttonElement.addEventListener('click', buttonHandler);
